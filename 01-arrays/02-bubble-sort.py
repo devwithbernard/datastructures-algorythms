93e635b4e5f16b7a1909c655b878a10eaa7341c1
Bernard Konan <devwithbernard@gmail.com>
2025-10-02 15:36:03 +0000

    Improve a little bit bubble sort algorythm

--- 01-arrays/02-bubble-sort.py
+++ 01-arrays/02-bubble-sort.py
@@ -10,10 +10,15 @@ def bubble_sort(items: list[int]) -> list[int]:
     n = len(items)
     
     for i in range(n - 1):
+        swapped = False
+        
         for j in range(n - i - 1):
             if items[j] > items[j + 1]:
                 items[j], items[j + 1] = items[j + 1], items[j]
-
+                swapped = True
+        if not swapped:
+            break
+        
     return items
 
 print(bubble_sort(numbers))
