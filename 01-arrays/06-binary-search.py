eae43a83fd3ce9e809e8eb64af7a135109430958
Bernard Konan <devwithbernard@gmail.com>
2025-10-02 23:04:34 +0000

    Array: Binary search

---
+++ 01-arrays/06-binary-search.py
@@ -0,0 +1,25 @@
+"""
+Array: Binary search
+"""
+
+def binary_search(arr, target):
+    left = 0
+    right = len(arr) - 1
+
+    while left <= right:
+        mid = (left + right) // 2
+
+        if arr[mid] == target:
+            return mid
+        
+        if arr[mid] < target:
+            left = mid + 1
+        else:
+            right = mid - 1
+    
+    return -1
+
+
+myArray = [1, 3, 5, 7, 9, 11, 13, 15, 17, 19]
+myTarget = 15
+print(f"binary search:", binary_search(myArray, myTarget))
