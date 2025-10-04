26894f5587610b5466ea8a5ddaaa0d032c0fb09b
Bernard Konan <devwithbernard@gmail.com>
2025-10-04 14:52:50 +0000

    Linked Lists: implementation of linked list using python

---
+++ linked-lists/linked-lists.py
@@ -0,0 +1,23 @@
+# Implement Linked-List in python
+
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+node1 = Node(1)
+node2 = Node(2)
+node3 = Node(3)
+node4 = Node(4)
+
+node1.next = node2
+node2.next = node3
+node3.next = node4
+
+current_node = node1
+
+while current_node:
+    print(current_node.data, end=" -> ")
+    current_node = current_node.next
+
+print("Null")
