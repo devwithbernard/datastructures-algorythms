f8a8abbeecfc21e56ff4ab38d0c3a29ecce4bdfe
Bernard Konan <devwithbernard@gmail.com>
2025-10-06 11:57:58 +0000

    Stacks: Implement stack using class

--- 03-stacks-queues/stacks.py
+++ 03-stacks-queues/stacks.py
@@ -26,3 +26,47 @@ print("Is empty:", is_empty)
 # Size: Return the size of the stack
 size = len(stack)
 print("Size:", size)
+
+# Implement Stack using class
+
+class Stack:
+    def __init__(self):
+        self.stacks = []
+
+    def push(self, element):
+        self.stacks.append(element)
+
+    def pop(self):
+        if self.is_empty():
+            return "Stack is empty"
+
+        return self.stacks.pop()
+
+    def peek(self):
+        if self.is_empty():
+            return "Stack is empty"
+
+        return self.stacks[-1]
+
+    def is_empty(self):
+        return len(self.stacks) == 0
+
+    def size(self):
+        return len(self.stacks)
+
+
+# Create a stack
+myStack = Stack()
+
+myStack.push('A')
+myStack.push('B')
+myStack.push('C')
+print("Stack: ", myStack.stacks)
+
+print("Pop: ", myStack.pop())
+
+print("Peek: ", myStack.peek())
+
+print("isEmpty: ", myStack.is_empty())
+
+print("Size: ", myStack.size())
