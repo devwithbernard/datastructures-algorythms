325029a38abed28033a46a2bfb1d22c859d862f9
Bernard Konan <devwithbernard@gmail.com>
2025-10-06 10:18:24 +0000

    Linked lists: traverse and print nodes

--- 02-linked-lists/linked-list-operations.py
+++ 02-linked-lists/linked-list-operations.py
@@ -9,6 +9,17 @@ class Node:
         self.data = data
         self.next = None
 
+
+def traverse_list(head: Node):
+    current_node = head
+
+    while current_node:
+        print(current_node.data, end=' -> ')
+        current_node = current_node.next
+
+    print("Null")
+
+
 def find_lowest_value(head: Node):
     min_val = head.data
     current_node: Node | None = head.next
@@ -41,6 +52,8 @@ node4 = Node(9)
 node1.next = node2
 node2.next = node3
 node3.next = node4
+
+traverse_list(node1)
 
 print("Lowest value:", find_lowest_value(node1))
 print("Largest value:", find_largest_value(node1))
