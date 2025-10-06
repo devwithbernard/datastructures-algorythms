6776006c782f75e4b59773cdb6c6ce7f87579854
Bernard Konan <devwithbernard@gmail.com>
2025-10-06 12:24:47 +0000

    Queues: Implement a queue using Python List

--- 03-stacks-queues/queues.py
+++ 03-stacks-queues/queues.py
@@ -13,3 +13,31 @@ Peek: Returns the first element in the queue.
 isEmpty: Checks if the queue is empty.
 Size: Finds the number of elements in the queue.
 """
+
+# Implement queue using Python List
+
+queue = []
+
+# Enqueue : Add a new element to the queue
+queue.append("A")
+queue.append("B")
+queue.append("C")
+queue.append("D")
+
+print("Queue:", queue)
+
+# Dequeue: Removes and returns the first (front) element from the queue
+element = queue.pop(0)
+print("Dequeue:", element)
+
+# Peek: Returns the first element in the queue
+element = queue[0]
+print("Peek:", element)
+
+# Is_empty: Checks if the queue is empty
+is_empty = len(queue) == 0
+print("Is empty:", is_empty)
+
+# Size: Finds the number of elements in the queue
+size = len(queue)
+print("Size:", size)
